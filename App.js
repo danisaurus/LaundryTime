3242195ac2984322329f2b8f22fab6db65df366d
danisaurus <danidirks@gmail.com>
2021-04-28 12:04:58 +0800

    adding theming to application

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
+import {
+  useFonts as useRaleway,
+  Raleway_400Regular,
+} from "@expo-google-fonts/raleway";
+
+import {
+  useFonts as useFredoka,
+  FredokaOne_400Regular,
+} from "@expo-google-fonts/fredoka-one";
+
+import { ThemeProvider } from "styled-components/native";
+
+import { theme } from "./src/utils/theme";
 
 export default function App() {
+  const [ralewayLoaded] = useRaleway({
+    Raleway_400Regular,
+  });
+
+  const [fredokaLoaded] = useFredoka({
+    FredokaOne_400Regular,
+  });
+
+  if (!ralewayLoaded || !fredokaLoaded) {
+    return null;
+  }
+
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Our React Native Blank Canvas</Text>
-      <Text>make shit happen on this fucking app</Text>
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <Text>Our React Native Blank Canvas</Text>
+          <Text>make shit happen on this fucking app</Text>
+        </View>
+      </ThemeProvider>
+    </>
   );
 }
